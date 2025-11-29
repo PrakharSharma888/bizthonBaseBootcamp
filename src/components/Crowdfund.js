0ed4830c8f9ac6c9bbb174b5460aa0d4e00885cb
prakhar5 <prakhar@ith.tech>
2025-11-29 12:28:41 +0530

    Added: Two more functions

--- src/components/Crowdfund.js
+++ src/components/Crowdfund.js
@@ -1,13 +1,183 @@
 import react from "react";
-import {ethers} from 'ethers'
+import { ethers } from "ethers";
+import { useState } from "react";
 
-async function Crowdfund() {
-    const contractAddress = "0xF27374C91BF602603AC5C9DaCC19BE431E3501cb"
-    const provider = new ethers.BrowserProvider(window.ethereum)
-    const signer = await provider.getSigner()
-    console.log(signer)
+function Crowdfund() {
+  const [balance, setBalance] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
 
-  return <h1>Hello Shivam</h1>;
+  const contractAddress = "0x43f6101505BCA7eF2E6bBb330bEb121513E19b99";
+
+  async function main() {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+
+    const abi = [
+      {
+        inputs: [],
+        name: "endFunding",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "setFund",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "_endTime",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "_goalAmount",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "nonpayable",
+        type: "constructor",
+      },
+      {
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+        ],
+        name: "withdrawalSomeFunds",
+        outputs: [
+          {
+            internalType: "bool",
+            name: "",
+            type: "bool",
+          },
+        ],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "withdrawlAll",
+        outputs: [
+          {
+            internalType: "bool",
+            name: "",
+            type: "bool",
+          },
+        ],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "checkAllFunds",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "address",
+            name: "myAddress",
+            type: "address",
+          },
+        ],
+        name: "checkYourFunds",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "endTime",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "goalAmount",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "isStarted",
+        outputs: [
+          {
+            internalType: "bool",
+            name: "",
+            type: "bool",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+    ];
+
+    const signer1 = await provider.getSigner();
+    setSigner(signer1);
+    const crowdFundContract = new ethers.Contract(contractAddress, abi, signer);
+    setContract(crowdFundContract);
+  }
+
+  async function handleBalanceCheck() {
+    const balanceAll = await crowdFundContract.checkAllFunds();
+    setBalance(balanceAll.toString());
+    console.log(balanceAll.toString());
+
+    console.log(crowdFundContract);
+  }
+
+  async function handleYourBalance() {
+    const yourFunds = await crowdFundContract.checkYourFunds();
+    console.log(yourFunds);
+  }
+
+  main();
+
+  return (
+    <>
+      <button onClick={handleBalanceCheck}>Get Balance</button>
+      <button onClick={handleYourBalance}>Get Your Balance</button>
+      <h1>{balance}</h1>
+    </>
+  );
 }
 
 export default Crowdfund;
