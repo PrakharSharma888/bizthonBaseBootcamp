59106304d4309f0d9c6512ab8ba43e522da6d2c9
prakhar5 <prakhar@ith.tech>
2025-11-30 12:16:01 +0530

    Added: CSS using bootstrap

--- src/components/Crowdfund.js
+++ src/components/Crowdfund.js
@@ -8,15 +8,18 @@ function Crowdfund() {
   const [signer, setSigner] = useState(null);
   const [signer2, setSigner2] = useState(null);
   const [contract, setContract] = useState(null);
-  const [contract2, setContract2] = useState(null) 
-  const [yourBalance, setYourBalance] = useState(null)
-
+  const [contract2, setContract2] = useState(null);
+  const [yourBalance, setYourBalance] = useState(null);
+  const [accounts, setAccounts] = useState(null);
 
   const contractAddress = "0x5D6A7dd379D2b0015CCEE863AB9FeCf8911713C1";
 
   async function main() {
-    const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
     console.log(accounts);
+    setAccounts(accounts[0]);
     const provider1 = new ethers.BrowserProvider(window.ethereum);
     console.log(provider1);
     setProvider(provider);
@@ -160,11 +163,18 @@ function Crowdfund() {
     const signer1 = await provider1.getSigner(accounts[0]);
     const signer2 = await provider1.getSigner(accounts[1]);
     setSigner2(signer2);
-    console.log("sdsdsdsd",signer2)
     setSigner(signer1);
-    const crowdFundContract = new ethers.Contract(contractAddress, abi, signer1);
-    const crowdFundContrac2 = new ethers.Contract(contractAddress, abi, signer2);
-    setContract2(crowdFundContrac2)
+    const crowdFundContract = new ethers.Contract(
+      contractAddress,
+      abi,
+      signer1
+    );
+    const crowdFundContrac2 = new ethers.Contract(
+      contractAddress,
+      abi,
+      signer2
+    );
+    setContract2(crowdFundContrac2);
     setContract(crowdFundContract);
   }
 
@@ -175,34 +185,81 @@ function Crowdfund() {
   }
 
   async function handleYourBalance() {
-    const yourFunds = await contract.checkYourFunds("0x457124f461616c514EFd2A182526e630B4bE3c4b");
-    setYourBalance(yourFunds)
+    const yourFunds = await contract.checkYourFunds(provider.getAddress());
+    setYourBalance(yourFunds);
     console.log(yourFunds);
   }
 
-  async function handleSetFund(){
-    const txn = await contract2.setFund({value: ethers.parseUnits("0.00001", 18)})
-    await txn.wait()
-    console.log(txn)
+  async function handleSetFund(e) {
+    const txn = await contract2.setFund({
+      value: ethers.parseUnits(e.value, 18),
+    });
+    await txn.wait();
+    console.log(txn);
   }
 
   async function withdrawAll() {
     const txn = await contract2.withdrawlAll();
-    await txn.wait()
-    console.log(txn)
+    await txn.wait();
+    console.log(txn);
   }
 
-
+  function shortenAddress(addr) {
+    if (!addr) return "";
+    return addr.slice(0, 6) + "..." + addr.slice(-4);
+  }
 
   return (
     <>
-    <button onClick={main}>Connect Wallet</button>
-      <button onClick={handleBalanceCheck}>Get Balance</button>
-      <button onClick={handleYourBalance}>Get Your Balance</button>
-      <button onClick={handleSetFund}>Donate the crowdFund</button>
-      <button onClick={withdrawAll}>withdrawAll</button>
-      <h1>Total Balance{balance}</h1>
-      <h1>Total Balance{yourBalance}</h1>
+      <nav class="navbar bg-body-tertiary">
+        <form class="container-fluid justify-content-start">
+          <button
+            class="btn btn-outline-success me-2"
+            onClick={main}
+            type="button"
+          >
+            Connect Wallet
+          </button>
+          <button
+            class="btn btn-sm btn-outline-secondary m-3"
+            onClick={handleBalanceCheck}
+            type="button"
+          >
+            Get Balance
+          </button>
+          <button
+            class="btn btn-sm btn-outline-secondary"
+            onClick={handleYourBalance}
+            type="button"
+          >
+            Get Your Balance
+          </button>
+          <h5 className="ms-5">{shortenAddress(accounts)}</h5>
+        </form>
+      </nav>
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <div class="input-group mb-3 mt-5">
+          <img className="rounded mx-auto d-block" src="./fund.jpg"/>
+          <span class="input-group-text">₹</span>
+
+          <input
+            type="text"
+            class="form-control"
+            aria-label="Amount (to the nearest rupees)"
+          />
+          <span class="input-group-text">.00</span>
+        </div>
+        <button className="btn btn-outline-danger m-3" onClick={handleSetFund}>
+          Donate the crowdFund
+        </button>
+        <button className="btn btn-outline-danger" onClick={withdrawAll}>
+          Withdraw All
+        </button>
+      </div>
+      <div class="position-absolute top-100 start-50 translate-middle">
+        <h3>Total Balance{balance}</h3>
+        <h3>Your Total Balance{yourBalance}</h3>
+      </div>
     </>
   );
 }
